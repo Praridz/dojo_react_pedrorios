f97b303f96390ca41ae347f404b69a07dd61aeaf
Praridz <rios.371@hotmail.com>
2020-02-22 04:49:21 -0500

    final commit del dojo de react Funcionando

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from './containers/Layout/Layout'
+import Cards from './components/Cards/Cards'
+import Task from './containers/Tasks/Task'
+
+function App() {
+  return (
+    <div>
+
+      <Layout>
+        <Task/>
+      </Layout>
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
